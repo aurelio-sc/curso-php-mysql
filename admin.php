c102f215a9045682c2df6d407f96892a3a5dea3c
Aurélio Soares <asc.mat@gmail.com>
2024-05-21 11:29:16 -0300

    lista todos os produtos na view de admin

--- admin.php
+++ admin.php
@@ -1,3 +1,12 @@
+<?php
+  require 'src/conexao-bd.php';
+  require 'src/Modelo/Produto.php';
+  require 'src/Repositorio/ProdutoRepositorio.php';
+
+  $produtoRepositorio = new ProdutoRepositorio($pdo);
+  $produtos = $produtoRepositorio->buscarTodos();
+?>
+
 <!doctype html>
 <html lang="pt-br">
 <head>
@@ -36,43 +45,20 @@
         </tr>
       </thead>
       <tbody>
-      <tr>
-        <td>Bife</td>
-        <td>Almoço</td>
-        <td>Delicioso prato</td>
-        <td>R$ 25.00</td>
-        <td><a class="botao-editar" href="editar-produto.html">Editar</a></td>
-        <td>
-          <form>
-            <input type="button" class="botao-excluir" value="Excluir">
-          </form>
-        </td>
-        
-      </tr>
-      <tr>
-        <td>Frango</td>
-        <td>Almoço</td>
-        <td>Delicioso prato</td>
-        <td>R$ 25.00</td>
-        <td><a class="botao-editar" href="editar-produto.html">Editar</a></td>
-        <td>
-          <form>
-            <input type="button" class="botao-excluir" value="Excluir">
-          </form>
-        </td>
-      </tr>
-      <tr>
-        <td>Café Gelado</td>
-        <td>Café</td>
-        <td>Delicioso prato</td>
-        <td>R$ 25.00</td>
-        <td><a class="botao-editar" href="editar-produto.html">Editar</a></td>
-        <td>
-          <form>
-            <input type="button" class="botao-excluir" value="Excluir">
-          </form>
-        </td>
-      </tr>
+      <?php foreach ($produtos as $produto): ?>      
+        <tr>
+          <td><?php echo $produto->getNome(); ?></td>
+          <td><?php echo $produto->getTipo(); ?></td>
+          <td><?php echo $produto->getDescricao(); ?></td>
+          <td><?php echo $produto->getPrecoFormatado(); ?>0</td>
+          <td><a class="botao-editar" href="editar-produto.html">Editar</a></td>
+          <td>
+            <form>
+              <input type="button" class="botao-excluir" value="Excluir">
+            </form>
+          </td>        
+        </tr>
+      <?php endforeach; ?>
       </tbody>
     </table>
   <a class="botao-cadastrar" href="cadastrar-produto.html">Cadastrar produto</a>
--- src/Repositorio/ProdutoRepositorio.php
+++ src/Repositorio/ProdutoRepositorio.php
@@ -27,4 +27,45 @@ class ProdutoRepositorio
         
         return $dadosCafe;
     }
+
+    public function opcoesAlmoco(): array
+    {
+        $sql = 'SELECT * FROM produtos WHERE tipo = "Almoço" ORDER BY preco';
+        $statement = $this->pdo->query($sql);    
+        $produtosAlmoco = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        $dadosAlmoco = array_map(function($almoco) {
+            return new Produto(
+                        $almoco['id'], 
+                        $almoco['tipo'], 
+                        $almoco['nome'],  
+                        $almoco['descricao'], 
+                        $almoco['imagem'], 
+                        $almoco['preco']
+            );
+        }, $produtosAlmoco);
+        
+        return $dadosAlmoco;
+    }
+
+    public function buscarTodos(): array
+    {
+        $sql = 'SELECT * FROM produtos ORDER BY tipo';
+        $statement = $this->pdo->query($sql);    
+        $produtos = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        $dados = array_map(function($produto) {
+            return new Produto(
+                        $produto['id'], 
+                        $produto['tipo'], 
+                        $produto['nome'],  
+                        $produto['descricao'], 
+                        $produto['imagem'], 
+                        $produto['preco']
+            );
+        }, $produtos);
+        
+        return $dados;
+    }
+    
 }
